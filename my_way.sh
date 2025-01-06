da1e1557c89fdf6e0edcb042c8d206d3eb4ba368
Lucas Nardon <lucaslnardon@gmail.com>
2025-01-06 00:42:01 -0300

    Update bash script to download latest nvim from official repo

--- my_way.sh
+++ my_way.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo snap install nvim && mv ./* ~/.config/nvim
+
+# Download the latest stable neovim from the official repo
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+# Move config to folder
+mv ./* ~/.config/nvim
