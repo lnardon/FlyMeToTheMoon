563a8afdf153ebf1ab8cd5f36736cd9441e1019f
Lucas Nardon <43593024+lnardon@users.noreply.github.com>
2025-01-07 12:43:42 -0300

    Remove sudo

--- my_way.sh
+++ my_way.sh
@@ -2,8 +2,8 @@
 
 # Download the latest stable neovim from the official repo
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm -rf /opt/nvim
+tar -C /opt -xzf nvim-linux64.tar.gz
 
 # Move config to folder
 mv ./* ~/.config/nvim
